86d622d9379ffb620059438fca45eb9b7b9bcc7f
your_username <nguyenthanhloi260303@gmail.com>
2023-09-19 07:14:12 +0700

    19/09/2023

--- src/main/java/com/example/demo_thanhloi26/HelloServlet.java
+++ src/main/java/com/example/demo_thanhloi26/HelloServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message; //mess
+    private String message;
 
     public void init() {
         message = "Hello World!";
